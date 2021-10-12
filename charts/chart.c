fb21123bcf7ac00eca351c0febd086fc31d2fd3b
kaikaewkmitl <64011456@kmitl.ac.th>
2021-10-12 13:33:58 +0700

    add color to chart

--- charts/chart.c
+++ charts/chart.c
@@ -1,5 +1,14 @@
 #include "chart.h"
 
+#define COLOR_RED "\033[31m"
+#define COLOR_GREEN "\033[32m"
+#define COLOR_YELLOW "\033[33m"
+#define COLOR_BLUE "\033[34m"
+#define COLOR_MAGENTA "\033[35m"
+#define COLOR_CYAN "\033[36m"
+#define COLOR_WHITE "\033[37m"
+#define COLOR_RESET "\033[0m"
+
 char *COLUMNS[N_COLUMNS] = {"Keys", "Count", "Chart"};
 
 Chart *newChart(char *title)
@@ -36,24 +45,26 @@ void display(Chart *ch)
     updateColumn2Width(ch);
     updateColumn3Width(ch);
 
-    printf("\n %s\n\n", ch->title);
+    printf("\n %s%s\n\n", COLOR_WHITE, ch->title);
 
     printHeader(ch);
     printKeys(ch);
     printFooter(ch);
+
+    printf("%s", COLOR_RESET);
 }
 
 void printHeader(Chart *ch)
 {
-    printf(" Keys ");
+    printf(" %sKeys%s ", COLOR_GREEN, COLOR_RESET);
     int padding = ch->columnWidth[0] - strlen(COLUMNS[0]);
     printCharNTimes(' ', padding);
 
-    printf("| Count ");
+    printf("%s| %sCount%s ", COLOR_WHITE, COLOR_CYAN, COLOR_RESET);
     padding = ch->columnWidth[1] - strlen(COLUMNS[1]);
     printCharNTimes(' ', padding);
 
-    printf("| Chart \n");
+    printf("%s| %sChart%s \n", COLOR_WHITE, COLOR_YELLOW, COLOR_RESET);
 
     printNewRow(ch);
 }
@@ -68,22 +79,23 @@ void printKeys(Chart *ch)
 {
     for (int i = 0; i < ch->size; i++)
     {
-        printf(" %s ", ch->keys[i].keyName);
+        printf(" %s%s%s ", COLOR_GREEN, ch->keys[i].keyName, COLOR_RESET);
         int padding = ch->columnWidth[0] - strlen(ch->keys[i].keyName);
         printCharNTimes(' ', padding);
 
-        printf("| %d ", ch->keys[i].count);
+        printf("%s| %s%d%s ", COLOR_WHITE, COLOR_CYAN, ch->keys[i].count, COLOR_RESET);
         padding = ch->columnWidth[1] - ch->keys[i].countDigit;
         printCharNTimes(' ', padding);
 
-        printf("| ");
+        printf("%s| %s", COLOR_WHITE, COLOR_YELLOW);
         printCharNTimes('#', ch->keys[i].count);
-        printf(" \n");
+        printf("%s\n", COLOR_RESET);
     }
 }
 
 void printNewRow(Chart *ch)
 {
+    printf("%s", COLOR_WHITE);
     for (int i = 0; i < N_COLUMNS; i++)
     {
         printCharNTimes('-', ch->columnWidth[i] + 2);
@@ -93,7 +105,7 @@ void printNewRow(Chart *ch)
         }
     }
 
-    printf("\n");
+    printf("%s\n", COLOR_RESET);
 }
 
 void updateColumn1Width(Chart *ch)
