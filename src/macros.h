adaef8d24daddacb63e92a888605f973dd68b41a
kaikaewkmitl <64011456@kmitl.ac.th>
2021-10-28 21:21:16 +0700

    add CLEAR_LINE and use when PRINT_ERROR()

--- src/macros.h
+++ src/macros.h
@@ -8,6 +8,10 @@
 #define N_ASCII 128
 #define CHAR_LIMIT 50
 
+#define CURSOR_HOME "\033[H"
+#define CLEAR_SCREEN "\033[1J"
+#define CLEAR_LINE "\033[2K"
+
 #define COLOR_BLACK "\033[30m"
 #define COLOR_RED "\033[31m"
 #define COLOR_GREEN "\033[32m"
@@ -18,9 +22,6 @@
 #define COLOR_WHITE "\033[37m"
 #define COLOR_RESET "\033[0m"
 #define BOLD_TEXT "\033[1m"
-#define PRINT_ERROR(msg) printf("%s%sERROR: %s%s", COLOR_RED, BOLD_TEXT, msg, COLOR_RESET);
-
-#define CURSOR_HOME "\033[H"
-#define CLEAR_SCREEN "\033[1J"
+#define PRINT_ERROR(msg) printf("%s\r%s%sERROR: %s%s", CLEAR_LINE, COLOR_RED, BOLD_TEXT, msg, COLOR_RESET);
 
 #endif
